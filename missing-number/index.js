5905c4d4ef2f503bfd6d4a770ef52935c3f13026
Oluwatobi <krisella74@gmail.com>
2022-11-18 21:48:57 +0100

    faster implementation

--- missing-number/index.js
+++ missing-number/index.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
  var missingNumber = function(nums) {
-    let numsIndex = {}
+    let totalNums = 0
     for (let n of nums) {
-        numsIndex[n] = 1
+        totalNums += n;
     }
     
+    let supposedTotal = 0;
     for (let i=0; i<=nums.length; i++) {
-        if (numsIndex[i] !== 1) {
-            return i 
-        }
+        supposedTotal+= i
     }
+
+    return supposedTotal - totalNums
     
 };
